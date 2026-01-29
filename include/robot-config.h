50b3b0fe8a95f30d12cf43f1f8b1b98a18a23573
patronum213 <collinhanney@gmail.com>
2026-01-28 18:05:37 -1000

    implimented template project in mikLib

--- include/robot-config.h
+++ include/robot-config.h
@@ -1,20 +1,21 @@
-using namespace vex;
+#include "vex.h"
 
-extern brain Brain;
+class motor;
+class motor_group;
+class Assembly;
 
-// VEXcode devices
-extern controller Controller1;
-extern motor LeftMotor1;
-extern motor LeftMotor2;
-extern motor LeftMotor3;
-extern motor RightMotor1;
-extern motor RightMotor2;
-extern motor RightMotor3;
-extern motor IntakeMotor;
-extern motor OuttakeMotor;
-/**
- * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
- *
- * This should be called at the start of your int main function.
- */
-void vexcodeInit(void);
+extern vex::brain Brain;
+extern vex::controller Controller;
+extern vex::competition Competition;
+
+extern bool calibrating;
+extern bool force_calibrate_inertial;
+
+extern Assembly assembly;
+
+void init(void);
+void calibrate_inertial(void);
+
+void disable_user_control(bool stop_all_motors_ = false);
+void enable_user_control(void);
+bool control_disabled(void);
--- include/vex.h
+++ include/vex.h
@@ -1,26 +1,67 @@
-/*----------------------------------------------------------------------------*/
-/*                                                                            */
-/*    Module:       vex.h                                                     */
-/*    Author:       Vex Robotics                                              */
-/*    Created:      1 Feb 2019                                                */
-/*    Description:  Default header for V5 projects                            */
-/*                                                                            */
-/*----------------------------------------------------------------------------*/
-//
+#pragma once
+
+#include <vector>
 #include <math.h>
 #include <stdio.h>
+#include <cstdint>
+#include <climits>
 #include <stdlib.h>
+#include <cstdlib>
 #include <string.h>
+#include <algorithm>
+#include <functional>
+#include <random>
+#include <chrono>
+#include <unordered_map>
+#include <sstream>
+#include <iomanip>
+#include <memory>
+#include <atomic>
+#include <tuple>
+#include <sstream>
+#include <type_traits>
+#include <iostream> 
+#include <cstddef>
 
 #include "v5.h"
 #include "v5_vcs.h"
 
+#include "mikLib/globals.h"
+#include "mikLib/Drive/util.h"
+#include "mikLib/Devices/motors.h"
+#include "mikLib/Devices/distance.h"
+#include "mikLib/Devices/piston.h"
+#include "mikLib/Drive/odom.h"
+#include "mikLib/Drive/PID.h"
+#include "assembly.h"
+#include "mikLib/Drive/chassis.h"
+#include "autons.h"
 #include "robot-config.h"
 
-#define waitUntil(condition)                                                   \
-  do {                                                                         \
-    wait(5, msec);                                                             \
-  } while (!(condition))
+#include "mikLib/UI/graphics/drawable.h"
+#include "mikLib/UI/UI_component.h"
 
-#define repeat(iterations)                                                     \
-  for (int iterator = 0; iterator < iterations; iterator++)
+#include "mikLib/UI/UI_util.h"
+#include "mikLib/UI/graphics/image.h"
+#include "mikLib/UI/graphics/rectangle.h"
+#include "mikLib/UI/graphics/logo.h"
+#include "mikLib/UI/graphics/circle.h"
+#include "mikLib/UI/graphics/text.h"
+#include "mikLib/UI/graphics/pixel.h"
+#include "mikLib/UI/graphics/line.h"
+#include "mikLib/UI/graphics/group.h"
+
+#include "mikLib/UI/button.h"
+#include "mikLib/UI/toggle.h"
+#include "mikLib/UI/label.h"
+#include "mikLib/UI/graphic.h"
+#include "mikLib/UI/background.h"
+#include "mikLib/UI/textbox.h"
+#include "mikLib/UI/screen.h"
+#include "mikLib/UI/screens/console_screen.h"
+#include "mikLib/UI/screens/auton_screen.h"
+#include "mikLib/UI/screens/config_screen.h"
+#include "mikLib/UI/screens/graph_screen.h"
+
+#include "mikLib/UI/UI_manager.h"
+#include "test.h"
